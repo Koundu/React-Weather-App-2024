6ad7e81e0eef42873c270692633ce5578d50043c
Koundu <koundinya.g07@gmail.com>
2024-06-19 19:05:04 +0530

    Completed the Navbar Component

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const TopButtons = () => {
+  const cities = [
+    {
+      id: 1,
+      name: 'London',
+    },
+    {
+      id: 2,
+      name: 'Tokyo',
+    },
+    {
+      id: 3,
+      name: 'Sydney',
+    },
+    {
+      id: 4,
+      name: 'Paris',
+    },
+    {
+      id: 5,
+      name: 'Toronto',
+    }
+  ]
+  return (
+    <div className='flex items-center justify-around my-6'>
+      {
+        cities.map((city) => (
+          <button 
+            className='text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in'
+            key={city.id}
+            >{city.name}</button>
+        ))
+      }
+    </div>
+  )
+}
+
+export default TopButtons;
