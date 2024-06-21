73f49badd0097c76bb5f644c967f97fab9f23698
Koundu <koundinya.g07@gmail.com>
2024-06-21 14:27:05 +0530

    React-weather-app-2024

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,7 +1,7 @@
 import { DateTime } from "luxon";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/'
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const getWeatherData = (infoType, searchParams) => {
     const url = new URL(BASE_URL + infoType);
